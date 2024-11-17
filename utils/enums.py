b91aaa0fd89150838219417c7718533cf36ce43a
Tom-R-Schneider <business.tom.schneider@gmail.com>
2024-11-17 19:47:55 +0100

    more enums

--- utils/enums.py
+++ utils/enums.py
@@ -1,2 +1,14 @@
+from enum import Enum
+class VIDEO_TYPE_DETAILS:
+    def __init__(self, id_suffix, upload_time, upload_day):
+        self.ID_SUFFIX = id_suffix
+        self.UPLOAD_TIME = upload_time
+        self.UPLOAD_DAY = upload_day
 class VIDEO_TYPE:
-    WORD =  "word"
+    WORD = VIDEO_TYPE_DETAILS('word', '19:00:00', 0)
+
+
+class WORD_TYPE(Enum):   
+    NOUN = "Substantiv"
+    ADJECTIVE = "Adjektiv"
+    VERB = "Verb"
