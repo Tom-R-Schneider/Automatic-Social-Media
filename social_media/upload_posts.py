734e3d8856e7283d4c7db0f5aa7620019d85aec1
Tom-R-Schneider <business.tom.schneider@gmail.com>
2024-11-23 21:29:45 +0100

    added upload cap

--- social_media/upload_posts.py
+++ social_media/upload_posts.py
@@ -15,21 +15,30 @@ from social_media.Twitter import twitter
 content_path = os.path.join(os.getcwd(), 'create_content', 'created_content')
 content_file_path = os.path.join(content_path, 'content_data.json')
 
-import social_media.Youtube.youtube as youtube
-
 def start_upload_process():
     if os.path.isfile(content_file_path):
         with open(content_file_path) as f:
             content_json = json.load(f)
     
+    upload_cap_counter = 0
     for post_date in content_json:
         if date.fromisoformat(post_date) < date.today() or content_json[post_date]["uploaded_all"] == True: continue
-
+        upload_cap_counter += 1
         for upload_type in content_json[post_date]["uploaded"]:
             if upload_type == True: continue
+
             match upload_type:
                 case "youtube": content_json[post_date]["uploaded"][upload_type] = youtube.start_upload(content_json[post_date]["content"])
                 case "tiktok": content_json[post_date]["uploaded"][upload_type] = tiktok.start_upload(content_json[post_date]["content"])
                 case "facebook": content_json[post_date]["uploaded"][upload_type] = facebook.start_upload(content_json[post_date]["content"])
                 case "instagram": content_json[post_date]["uploaded"][upload_type] = instagram.start_upload(content_json[post_date]["content"])
                 case "twitter": content_json[post_date]["uploaded"][upload_type] = twitter.start_upload(content_json[post_date]["content"])
+
+        upload_all_check = True
+        for upload_type in content_json[post_date]["uploaded"]:
+            if upload_type == True: continue
+            upload_all_check = False
+        
+        content_json[post_date]["uploaded_all"] = upload_all_check
+        
+        if upload_cap_counter == 20: break
