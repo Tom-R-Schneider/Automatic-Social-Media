4efaf43b1ee66f4da460d11d29fb944302a2a063
Tom-R-Schneider <business.tom.schneider@gmail.com>
2024-11-24 00:03:30 +0100

    removed test var

--- social_media/Twitter/twitter_upload.py
+++ social_media/Twitter/twitter_upload.py
@@ -12,6 +12,6 @@ account = Account(cookies=credentials["cookies_path"])
 def start_upload(upload_data):
     vid_content_path = os.path.join(content_path, 'videos', upload_data["post_id"] + '_vid.mp4')
     try:
-        test = account.schedule_tweet(upload_data["post_title"], upload_data["upload_datetimeiso"].replace("T", " ")[:-3], media=[{'media': vid_content_path, 'alt': upload_data["post_id"]}])
+        account.schedule_tweet(upload_data["post_title"], upload_data["upload_datetimeiso"].replace("T", " ")[:-3], media=[{'media': vid_content_path, 'alt': upload_data["post_id"]}])
         return True
     except(): return False 
