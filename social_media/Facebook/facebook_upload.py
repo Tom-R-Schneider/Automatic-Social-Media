d6c2716ebf3ee9fa786400c7e5edde92a641576e
Tom-R-Schneider <business.tom.schneider@gmail.com>
2024-11-24 19:07:09 +0100

    code cleanup

--- social_media/Facebook/facebook_upload.py
+++ social_media/Facebook/facebook_upload.py
@@ -11,11 +11,11 @@ content_path = os.path.join(os.getcwd(), 'create_content', 'created_content')
 with open(credentials_path, 'r') as file:
     credentials = json.load(file)
 
+fb_page = fb.GraphAPI(credentials["access_token"])
+
 def start_upload(upload_data):
     img_content_path = os.path.join(content_path, 'images', upload_data["post_id"] + '_img.png')
-    fb_page = fb.GraphAPI(credentials["access_token"])
-    test = int(time.mktime(datetime.fromisoformat(upload_data["upload_datetimeiso"]).timetuple()))
-    post = fb_page.put_photo(open(img_content_path, "rb"), message="Testing", published= False, unpublished_content_type= "SCHEDULED", scheduled_publish_time= int(time.mktime(datetime.fromisoformat(upload_data["upload_datetimeiso"]).timetuple())))
+    post = fb_page.put_photo(open(img_content_path, "rb"), message=upload_data["post_title"], published= False, unpublished_content_type= "SCHEDULED", scheduled_publish_time= int(time.mktime(datetime.fromisoformat(upload_data["upload_datetimeiso"]).timetuple())))
     try: 
         if post["id"] != "": return True
         return False
